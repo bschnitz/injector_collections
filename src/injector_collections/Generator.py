1a1177998bfcc8199830d624fa4e9d3edaed9e17
Benjamin Schnitzler <bschnitzler@horstmann-it-consulting.de>
2024-05-21 13:49:57 +0200

    Bugfixes when recursively for @CollectionItem annotated classes. Breaking changes.

--- src/injector_collections/Generator.py
+++ src/injector_collections/Generator.py
@@ -1,13 +1,13 @@
 import os
 import pkgutil
 import importlib
-import importlib.util as iutil
 from types import ModuleType
 from typing import Any, Callable, Iterable, Type
 from jinja2 import FileSystemLoader
 from jinja2 import Environment
 from injector_collections.CollectionItem import CollectionItem
 import injector_collections
+from importlib import util
 
 class Generator:
     generatedCollectionsFileName = 'generated.py'
@@ -15,26 +15,32 @@ class Generator:
     def generate(
             self,
             inject: Callable,
-            collectionModule: ModuleType,
-            scanPathes: Iterable[str]
+            collectionModule: str,
+            scannedModules: Iterable[str]
             ):
         collectionModuleDirectory = self.getModuleDirectory(collectionModule)
         # empty collections module to avoid circular imports
         with open(f'{collectionModuleDirectory}/{self.generatedCollectionsFileName}', 'w') as f:
             pass
 
-        collectionMetadata = self.gatherCollectionMetadata(scanPathes)
+        collectionMetadata = self.gatherCollectionMetadata(scannedModules)
 
         with open(f'{collectionModuleDirectory}/{self.generatedCollectionsFileName}', 'w') as f:
             f.write(self.renderCollectionsTemplate(inject, collectionMetadata))
 
         # reload the collections module to make generated collections
         # visible when using import from other places
-        importlib.reload(collectionModule.generated)
-        importlib.reload(collectionModule)
+        # collectionModule = importlib.import_module(collectionModule)
+        # importlib.reload(collectionModule.generated)
+        # importlib.reload(collectionModule)
 
-    def getModuleDirectory(self, module: ModuleType) -> str:
-        modulePath = module.__file__
+    def getModuleDirectory(self, module: str|ModuleType) -> str:
+        print(module)
+        if isinstance(module, str):
+            moduleSpec = util.find_spec(module)
+            modulePath = moduleSpec.origin
+        else:
+            modulePath = module.__file__
         assert(modulePath is not None)
         return os.path.dirname(modulePath)
 
@@ -56,19 +62,33 @@ class Generator:
 
     def gatherCollectionMetadata(
             self,
-            scanPathes: Iterable[str],
+            scannedModules: Iterable[str],
             ) -> dict[Type, list[tuple[Any, Any]]]:
-        # Import all classes decorated with @CollectionItem. The import will
-        # trigger the __call__ method of the decorator and this will populate
-        # the metadata dict, which this method returns.
-        for modinfo in pkgutil.walk_packages(scanPathes):
-            spec = iutil.find_spec(modinfo.name)
-            assert(spec is not None)
-            filepath = spec.origin
-            assert(filepath is not None)
-            with open(filepath, 'r') as f:
-                if '@CollectionItem' in f.read():
-                    importlib.import_module(modinfo.name)
+        ''' Gather Metadata for Collection generation with template
+
+        Recursively walks through all modules in 'scannedModules' and gathers
+        metadata for every class decorated with '@CollectionItem'.
+        '''
+        for m in scannedModules:
+            for spec in self.walkModules(m):
+                with open(spec.origin, 'r') as f:
+                    if '@CollectionItem' in f.read():
+                        importlib.import_module(spec.name)
 
         return CollectionItem.getItems()
 
+    def walkModules(self, rootModule: str):
+        info = util.find_spec(rootModule)
+        yield info
+
+        if info.submodule_search_locations is None:
+            return
+
+        #for modinfo in pkgutil.walk_packages(info.submodule_search_locations):
+        for modinfo in pkgutil.iter_modules(info.submodule_search_locations):
+            name = f'{info.name}.{modinfo.name}'
+            yield util.find_spec(name)
+            submods = self.walkModules(name)
+            next(submods) # remove the root, which was just yielded
+            for mi in submods:
+                yield mi
