3ff5d2f068f982e7a88dfced089bedb398818fea
Benjamin Schnitzler <bschnitzler@horstmann-it-consulting.de>
2024-05-30 09:04:01 +0200

    Type hinting for items in Collection (e.g. with Interfaces)

--- src/injector_collections/Collection.py
+++ src/injector_collections/Collection.py
@@ -1,20 +1,24 @@
-from typing import Type
+from typing import Any, Type
 
 class Collection:
     def __init__(self):
-        self._items: dict[Type, object] = {}
-        self._byClassname: dict[str, object] = {}
+        self._items: dict[Type, Any] = {}
+        self._byClassname: dict[str, Any] = {}
 
     @property
-    def items(self) -> dict[Type, object]:
+    def items(self) -> dict[Type, Any]:
         return self._items
 
-    def __getitem__(self, key):
+    def __getitem__(self, key) -> Any:
         return self._items[key]
 
-    def __setitem__(self, key, item):
+    def __setitem__(self, key, item: Any):
         self._items[key] = item
 
     @property
-    def byClassname(self) -> dict[str, object]:
+    def byClassname(self) -> dict[str, Any]:
         return self._byClassname
+
+    @classmethod
+    def getItemType(cls):
+        return Any
