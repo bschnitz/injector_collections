a11117cd06958fac30980e8f0b933bead6373d1f
Benjamin Schnitzler <bschnitzler@horstmann-it-consulting.de>
2025-03-28 14:37:21 +0100

    version 0.0.24 - better typeinfo for Collection

--- src/injector_collections/Collection.py
+++ src/injector_collections/Collection.py
@@ -1,27 +1,27 @@
-from typing import Any, Type
+from typing import Any
 
-class Collection:
+class Collection[T]:
     def __init__(self):
-        self._items: dict[Type, Any] = {}
-        self._byClassname: dict[str, Any] = {}
+        self._items: dict[type[T], T] = {}
+        self._byClassname: dict[str, T] = {}
 
     @property
-    def items(self) -> dict[Type, Any]:
+    def items(self) -> dict[type[T], T]:
         return self._items
 
     def __contains__(self, key):
         return key in self._items
 
-    def __getitem__(self, key) -> Any:
+    def __getitem__(self, key) -> T:
         return self._items[key]
 
-    def __setitem__(self, key, item: Any):
+    def __setitem__(self, key, item: T):
         self._items[key] = item
 
     @property
-    def byClassname(self) -> dict[str, Any]:
+    def byClassname(self) -> dict[str, T]:
         return self._byClassname
 
     @classmethod
-    def getItemType(cls):
+    def getItemType(cls) -> type[T]:
         return Any
